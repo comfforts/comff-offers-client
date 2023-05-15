f099b64ae381ca4d98f69094699487e49abd6961
Vishal Talwar <vtalwar.w@gmail.com>
2023-05-15 06:49:42 -0700

    adds offer CRUD methods

--- client.go
+++ client.go
@@ -1,10 +1,17 @@
 package client
 
 import (
+	"context"
+	"fmt"
+	"os"
 	"time"
 
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
 
+	config "github.com/comfforts/comff-config"
 	"github.com/comfforts/logger"
 
 	api "github.com/comfforts/comff-offers/api/v1"
@@ -28,6 +35,17 @@ var (
 const DeliveryClientContextKey = ContextKey("offers-client")
 const DefaultClientName = "comfforts-offers-client"
 
+type Client interface {
+	GetOfferStatuses(ctx context.Context, req *api.OfferStatusesRequest, opts ...grpc.CallOption) (*api.OfferStatusesResponse, error)
+	GetOfferTypes(ctx context.Context, req *api.OfferTypesRequest, opts ...grpc.CallOption) (*api.OfferTypesResponse, error)
+	CreateOffer(ctx context.Context, req *api.CreateOfferRequest, opts ...grpc.CallOption) (*api.OfferResponse, error)
+	UpdateOffer(ctx context.Context, req *api.UpdateOfferRequest, opts ...grpc.CallOption) (*api.OfferResponse, error)
+	GetOffer(ctx context.Context, req *api.GetOfferRequest, opts ...grpc.CallOption) (*api.OfferResponse, error)
+	GetOffers(ctx context.Context, req *api.GetOffersRequest, opts ...grpc.CallOption) (*api.OffersResponse, error)
+	DeleteOffer(ctx context.Context, req *api.DeleteOfferRequest, opts ...grpc.CallOption) (*api.DeleteResponse, error)
+	Close() error
+}
+
 type ClientOption struct {
 	DialTimeout      time.Duration
 	KeepAlive        time.Duration
@@ -48,4 +66,148 @@ type offersClient struct {
 	client api.OffersClient
 	conn   *grpc.ClientConn
 	opts   *ClientOption
+}
+
+func NewClient(
+	logger logger.AppLogger,
+	clientOpts *ClientOption,
+) (*offersClient, error) {
+	if clientOpts.Caller == "" {
+		clientOpts.Caller = DefaultClientName
+	}
+
+	tlsConfig, err := config.SetupTLSConfig(&config.ConfigOpts{
+		Target: config.OFFERS_CLIENT,
+	})
+	if err != nil {
+		logger.Error("error setting offers service client TLS", zap.Error(err))
+		return nil, err
+	}
+	tlsCreds := credentials.NewTLS(tlsConfig)
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(tlsCreds),
+	}
+
+	servicePort := os.Getenv("OFFERS_SERVICE_PORT")
+	if servicePort == "" {
+		servicePort = DEFAULT_SERVICE_PORT
+	}
+	serviceHost := os.Getenv("OFFERS_SERVICE_HOST")
+	if serviceHost == "" {
+		serviceHost = DEFAULT_SERVICE_HOST
+	}
+	serviceAddr := fmt.Sprintf("%s:%s", serviceHost, servicePort)
+	// with load balancer
+	// serviceAddr = fmt.Sprintf("%s:///%s", loadbalance.ShopResolverName, serviceAddr)
+	// serviceAddr = fmt.Sprintf("%s:///%s", "shops", serviceAddr)
+
+	conn, err := grpc.Dial(serviceAddr, opts...)
+	if err != nil {
+		logger.Error("offers client failed to connect", zap.Error(err))
+		return nil, err
+	}
+
+	client := api.NewOffersClient(conn)
+	logger.Info("offers client connected", zap.String("host", serviceHost), zap.String("port", servicePort))
+	return &offersClient{
+		client: client,
+		logger: logger,
+		conn:   conn,
+		opts:   clientOpts,
+	}, nil
+}
+
+func (ofc *offersClient) GetOfferStatuses(
+	ctx context.Context,
+	req *api.OfferStatusesRequest,
+	opts ...grpc.CallOption,
+) (*api.OfferStatusesResponse, error) {
+	ctx, cancel := ofc.contextWithOptions(ctx, ofc.opts)
+	defer cancel()
+
+	return ofc.client.GetOfferStatuses(ctx, req)
+}
+
+func (ofc *offersClient) GetOfferTypes(
+	ctx context.Context,
+	req *api.OfferTypesRequest,
+	opts ...grpc.CallOption,
+) (*api.OfferTypesResponse, error) {
+	ctx, cancel := ofc.contextWithOptions(ctx, ofc.opts)
+	defer cancel()
+
+	return ofc.client.GetOfferTypes(ctx, req)
+}
+
+func (ofc *offersClient) CreateOffer(
+	ctx context.Context,
+	req *api.CreateOfferRequest,
+	opts ...grpc.CallOption,
+) (*api.OfferResponse, error) {
+	ctx, cancel := ofc.contextWithOptions(ctx, ofc.opts)
+	defer cancel()
+
+	return ofc.client.CreateOffer(ctx, req)
+}
+
+func (ofc *offersClient) UpdateOffer(
+	ctx context.Context,
+	req *api.UpdateOfferRequest,
+	opts ...grpc.CallOption,
+) (*api.OfferResponse, error) {
+	ctx, cancel := ofc.contextWithOptions(ctx, ofc.opts)
+	defer cancel()
+
+	return ofc.client.UpdateOffer(ctx, req)
+}
+
+func (ofc *offersClient) GetOffer(
+	ctx context.Context,
+	req *api.GetOfferRequest,
+	opts ...grpc.CallOption,
+) (*api.OfferResponse, error) {
+	ctx, cancel := ofc.contextWithOptions(ctx, ofc.opts)
+	defer cancel()
+
+	return ofc.client.GetOffer(ctx, req)
+}
+
+func (ofc *offersClient) GetOffers(
+	ctx context.Context,
+	req *api.GetOffersRequest,
+	opts ...grpc.CallOption,
+) (*api.OffersResponse, error) {
+	ctx, cancel := ofc.contextWithOptions(ctx, ofc.opts)
+	defer cancel()
+
+	return ofc.client.GetOffers(ctx, req)
+}
+
+func (ofc *offersClient) DeleteOffer(
+	ctx context.Context,
+	req *api.DeleteOfferRequest,
+	opts ...grpc.CallOption,
+) (*api.DeleteResponse, error) {
+	ctx, cancel := ofc.contextWithOptions(ctx, ofc.opts)
+	defer cancel()
+
+	return ofc.client.DeleteOffer(ctx, req)
+}
+
+func (ofc *offersClient) Close() error {
+	if err := ofc.conn.Close(); err != nil {
+		ofc.logger.Error("error closing offers client connection", zap.Error(err))
+		return err
+	}
+	return nil
+}
+
+func (ofc *offersClient) contextWithOptions(ctx context.Context, opts *ClientOption) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithTimeout(ctx, ofc.opts.DialTimeout)
+	if ofc.opts.Caller != "" {
+		md := metadata.New(map[string]string{"service-client": ofc.opts.Caller})
+		ctx = metadata.NewOutgoingContext(ctx, md)
+	}
+
+	return ctx, cancel
 }
--- client_test.go
+++ client_test.go
@@ -1,1 +1,139 @@
 package client_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	comffC "github.com/comfforts/comff-constants"
+	offclient "github.com/comfforts/comff-offers-client"
+	api "github.com/comfforts/comff-offers/api/v1"
+	"github.com/comfforts/logger"
+)
+
+const TEST_DIR = "data"
+
+func TestOffersClient(t *testing.T) {
+	logger := logger.NewTestAppLogger(TEST_DIR)
+
+	for scenario, fn := range map[string]func(
+		t *testing.T,
+		ofc offclient.Client,
+	){
+		"test database setup check, succeeds": testDatabaseSetup,
+		"test offer CRUD, succeeds":           testOfferCRUD,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			ofc, teardown := setup(t, logger)
+			defer teardown()
+			fn(t, ofc)
+		})
+	}
+
+}
+
+func setup(t *testing.T, logger logger.AppLogger) (
+	dc offclient.Client,
+	teardown func(),
+) {
+	t.Helper()
+
+	clientOpts := offclient.NewDefaultClientOption()
+	clientOpts.Caller = "offers-client-test"
+
+	dc, err := offclient.NewClient(logger, clientOpts)
+	require.NoError(t, err)
+
+	return dc, func() {
+		t.Logf(" %s ended, will clean up", t.Name())
+		err := dc.Close()
+		require.NoError(t, err)
+	}
+}
+
+func testDatabaseSetup(t *testing.T, ofc offclient.Client) {
+	t.Helper()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	osResp, err := ofc.GetOfferStatuses(ctx, &api.OfferStatusesRequest{})
+	require.NoError(t, err)
+	require.Equal(t, len(osResp.Statuses), 9)
+
+	dsResp, err := ofc.GetOfferTypes(ctx, &api.OfferTypesRequest{})
+	require.NoError(t, err)
+	require.Equal(t, len(dsResp.Types), 1)
+}
+
+func testOfferCRUD(t *testing.T, ofc offclient.Client) {
+	t.Helper()
+
+	reqtr, shopId, courierId, deliveryId := "test-client-offer-crud@gmail.com", "test-client-offer-crud-shop", "test-client-offer-crud-courier", "CL1eCr341e0r620ff3r"
+	or := createOfferTester(t, ofc, &api.CreateOfferRequest{
+		ActorId:       shopId,
+		ParticipantId: courierId,
+		TransactionId: deliveryId,
+		RequestedBy:   reqtr,
+		Min:           comffC.F12,
+		Max:           comffC.F15,
+	})
+	or = getOfferTester(t, ofc, &api.GetOfferRequest{
+		Id: or.Offer.Id,
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	resp, err := ofc.UpdateOffer(ctx, &api.UpdateOfferRequest{
+		Id:          or.Offer.Id,
+		Status:      api.OfferStatus_EXPIRED,
+		RequestedBy: reqtr,
+	})
+	require.NoError(t, err)
+	assert.Equal(t, resp.Offer.Status, api.OfferStatus_EXPIRED, "offer status should be EXPIRED")
+
+	deleteOfferTester(t, ofc, &api.DeleteOfferRequest{
+		Id: or.Offer.Id,
+	})
+}
+
+func createOfferTester(t *testing.T, client offclient.Client, cor *api.CreateOfferRequest) *api.OfferResponse {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	resp, err := client.CreateOffer(ctx, cor)
+	require.NoError(t, err)
+	assert.Equal(t, resp.Offer.ActorId, cor.ActorId, "offer actor id should match input actor id")
+	assert.Equal(t, resp.Offer.ParticipantId, cor.ParticipantId, "offer participant id should match input participant id")
+	assert.Equal(t, resp.Offer.TransactionId, cor.TransactionId, "offer transaction id should match input transaction id")
+	assert.Equal(t, resp.Offer.Type, cor.Type, "offer type should match input type")
+	assert.Equal(t, resp.Offer.Status, api.OfferStatus_OPEN, "offer status should be OPEN")
+
+	assert.Equal(t, resp.Offer.Max, cor.Max, "offer max should match input max")
+	assert.Equal(t, resp.Offer.Min, cor.Min, "offer min should match input min")
+
+	return resp
+}
+
+func getOfferTester(t *testing.T, client offclient.Client, gor *api.GetOfferRequest) *api.OfferResponse {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	resp, err := client.GetOffer(ctx, gor)
+	require.NoError(t, err)
+	require.Equal(t, resp.Offer.Id, gor.Id)
+	return resp
+}
+
+func deleteOfferTester(t *testing.T, client offclient.Client, dor *api.DeleteOfferRequest) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	resp, err := client.DeleteOffer(ctx, dor)
+	require.NoError(t, err)
+	require.Equal(t, true, resp.Ok)
+}
